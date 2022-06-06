ca74e17f95f960e311325f35de535209f68e9337
Irvin <irvin999rafael6@csu.fullerton.edu>
2022-06-06 13:24:29 -0700

    Added SafeWalkVolunteer class in SafeWalkVolunteer.swift, and was able to run the program

--- SafeWalkVolunteer/Volunteer.swift
+++ SafeWalkVolunteer/Volunteer.swift
@@ -13,10 +13,32 @@ protocol Volunteer: ObservableObject {
     var maxHours: Int { get }
 }
 
-// TODO: Create a class called SafeWalkVolunteer that implementes the Volunteer protocol.
+class SafeWalkVolunteer : Volunteer {
+    var name: String
+    var age: Int
+    
+    // TODO: Create an Int maxHours computed property. It is a get-only property that returns 1 if the volunteer's age is less than 18 and returns 3 if the volunteer's age is 18 and over.
+    var maxHours: Int {
+        if age < 18 {
+            return 1
+        } else {
+            return 3
+        }
+    }
+    
+    // TODO: Create an initializer without parameters that sets name to an empty string and age to 0.
+    
+    init() {
+        self.name = ""
+        self.age = 0
+    }
+    
+    // TODO: Create another initializer that accepts a a String parameter called name and an Int parameter called age. Assign the parameter values to the corresponding properties of the class.
+ 
+    init(name: String, age: Int) {
+        self.name = name;
+        self.age = age;
+    }
+}
 
-// TODO: Create an Int maxHours computed property. It is a get-only property that returns 1 if the volunteer's age is less than 18 and returns 3 if the volunteer's age is 18 and over.
 
-// TODO: Create an initializer without parameters that sets name to an empty string and age to 0.
-
-// TODO: Create another initializer that accepts a a String parameter called name and an Int parameter called age. Assign the parameter values to the corresponding properties of the class.
